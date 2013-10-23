1e153025ed4133b4c1a09dcec4a9b56c73da2305
fomichev <fomichev@adv.ru>
2013-10-23 18:05:49 +0400

    add Jade templates

--- filter-catalog/js/all.js
+++ filter-catalog/js/all.js
@@ -1,9 +1,16 @@
 $(function () {
-    $('.js-widget').each(function () {
-        var hasOnclick = this.onclick;
-        var config = hasOnclick ? this.onclick() : {};
-        if (config.widget&&$.b[config.widget]) {
-            $(this)[config.widget](config.options ? config.options : null)
-        }
-    })
+
+    /** Обходим все виджеты в указанном блоке (по умолчанию: body) **/
+    $.initWidgets = function (block) {
+        $(block || document.body).find('.js-widget')
+            .each(function () {
+                var hasOnclick = this.onclick; // проверка наличия метода onclick
+                var config = hasOnclick ? this.onclick() : {}; // если метод есть, запускаем его
+                if (config.widget && $.fn[config.widget]) { // проверка наличия имени указанного виджета
+                    $(this)[config.widget](config.options ? config.options : null)
+                }
+            })
+    };
+
+    $.initWidgets(); // поиск и инициализация всех виджетов в элементе
 });
