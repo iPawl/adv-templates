baec0c72b840624d325cccb828c591e9cf77f165
iPawl <pashadesigner@mail.ru>
2013-10-24 15:44:26 +0400

    Добавил метод удаление виджитов + новый блок product

--- filter-catalog/blocks/b-catalog/b-catalog.js
+++ filter-catalog/blocks/b-catalog/b-catalog.js
@@ -7,11 +7,12 @@ $.widget('adv.catalog', {
     _render: function () {
         var _this = this,
             param = arguments[1].param;
-
+        $.destroyWidgets(_this.element);  // удаляем из кеша все данные виджета (события и т.п.)
         _this.element.append('<div class="b-catalog__loader"></div>');
-
         setTimeout(function () {
-            _this.element.load(_this.options.ajaxUrl + param);
+            _this.element.load(_this.options.ajaxUrl + param, function () {
+                $.initWidgets(_this.element); // инициализируем все виджеты в указанном блоке
+            });
         }, 300)
     }
 });
--- filter-catalog/js/all.js
+++ filter-catalog/js/all.js
@@ -1,6 +1,6 @@
 $(function () {
-
-    /** Обходим все виджеты в указанном блоке (по умолчанию: body) **/
+     //TODO поработать над упрощением кода
+    /** Инициализируем все виджеты в указанном блоке (по умолчанию: body) **/
     $.initWidgets = function (block) {
         $(block || document.body).find('.js-widget')
             .each(function () {
@@ -8,6 +8,18 @@ $(function () {
                 var config = hasOnclick ? this.onclick() : {}; // если метод есть, запускаем его
                 if (config.widget && $.fn[config.widget]) { // проверка наличия имени указанного виджета
                     $(this)[config.widget](config.options ? config.options : null)
+                }
+            })
+    };
+
+    /** Удаляем все виджеты в указанном блоке (по умолчанию: body) **/
+    $.destroyWidgets = function (block) {
+        $(block || document.body).find('.js-widget')
+            .each(function () {
+                var hasOnclick = this.onclick; // проверка наличия метода onclick
+                var config = hasOnclick ? this.onclick() : {}; // если метод есть, запускаем его
+                if (config.widget && $.fn[config.widget]) { // проверка наличия имени указанного виджета
+                    $(this)[config.widget]('destroy'); // удаляем из кеша все данные виджета (события и т.п.)
                 }
             })
     };
